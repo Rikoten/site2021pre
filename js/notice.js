6de4a1275bab26067fd71907959ad4d5e0ef6941
fukawatanabe <fu1101.wtnb@gmail.com>
2021-05-18 23:06:35 +0900

    contents

--- js/notice.js
+++ js/notice.js
@@ -1,12 +1,20 @@
 (async () => {
     const json = await fetch('/data/notice.json').then(res => res.json());
     const title = document.querySelector(".title");
-    const date = document.querySelector(".date")
+    const date = document.querySelector(".date");
+    const label = document.querySelector(".name");
+    const sentence = document.querySelector("p");
 
     for(const data of json){
-        console.log(data);
         title.textContent=data.head;
-        date.textConten=data.date;
+        date.textContent=data.date;
+        label.textContent=data.label;
+        const h = data.contents.length;
+        var all='';
+        for(let i=0; i < h; i++){
+            all += data.contents[i]+"\n";
+        }
+        sentence.innerHTML = all.replace(/\n/g, '<br>');
     }
 
 })();
