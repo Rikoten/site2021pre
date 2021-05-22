c274ed4a5b7205e0608d2899d8d09b0175b1aa16
fukawatanabe <fu1101.wtnb@gmail.com>
2021-05-22 13:07:30 +0900

    js

--- js/news.js
+++ js/news.js
@@ -1,17 +1,20 @@
 (async () => {
-    const json = await fetch('/data/news.json').then(res => res.json());
+    const json = await fetch('/data/notice.json').then(res => res.json());
     const title = document.querySelector(".title");
     const date = document.querySelector(".date");
     const label = document.querySelector(".name");
     const sentence = document.querySelector("p");
+    const link_title = document.querySelector("h2");
+    const link_name = document.querySelector("li");
+    /*const link = document.querySelector("a");*/
+    var ul = document.getElementById('link_list');
 
     for(const data of json){
         title.textContent=data.head;
         date.textContent=data.date;
         label.textContent=data.label;
-        const h = data.contents.length;
         var all='';
-        for(let i=0; i < h; i++){
+        for(let i=0; i < data.contents.length; i++){
             all += data.contents[i]+"\n";
         }
         sentence.innerHTML = all.replace(/\n/g, '<br>');
