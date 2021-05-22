6c2f4ec90653f9126c690c852985caf6022c1a24
soliste16 <loup.solitaire1192@gmail.com>
2021-05-22 16:06:29 +0900

    クイズまで対応

--- js/demopage.js
+++ js/demopage.js
@@ -7,19 +7,15 @@ const hoverAction = () => {
   const headerTop = document.querySelector("header .header-top");
   const desc = document.querySelector("header .desc");
   const iframe = document.querySelectorAll(".iframe-wrapper");
-  const h2 = document.querySelectorAll("h2");
-  const h3 = document.querySelectorAll("h3");
+  const h2 = document.querySelectorAll(".article h2");
+  const h3 = document.querySelectorAll(".article h3");
 
   elems.push({"item": headerTop, "name": "サムネイル＋企画名"});
   elems.push({"item": desc, "name": "企画紹介文"});
   elems.push({"item": iframe[0], "name": "メイン動画"});
   elems.push({"item": iframe[1], "name": "補助動画"});
-  elems.push({"item": h2[0], "name": "大見出し"});
-  elems.push({"item": h2[1], "name": "大見出し"});
-  elems.push({"item": h3[0], "name": "小見出し"});
-  elems.push({"item": h3[1], "name": "小見出し"});
-  elems.push({"item": h3[2], "name": "小見出し"});
-  elems.push({"item": h3[3], "name": "小見出し"});
+  for(const h of h2) elems.push({"item": h, "name": "大見出し"});
+  for(const h of h3) elems.push({"item": h, "name": "小見出し"});
   elems = elems.reverse();
 
   const hoverDiv = document.querySelectorAll(".hover-div");
@@ -30,9 +26,11 @@ const hoverAction = () => {
 
   for(const elem of elems) {
     if(elem.item && !elem.item.classList.contains("inactive")) {
-      const top = elem.item.getBoundingClientRect().top;
-      const height = elem.item.getBoundingClientRect().height == 0 ? 0 : elem.item.getBoundingClientRect().height + 20;
-      body.insertAdjacentHTML("beforeend", `<div class = "${classlist}" style = "height: ${height}px; top: ${top-10}px;"><div>${elem.name}</div></div>`);
+      if(elem.item.getBoundingClientRect().height !== 0) {
+        const top = elem.item.getBoundingClientRect().top;
+        const height = elem.item.getBoundingClientRect().height;
+        body.insertAdjacentHTML("beforeend", `<div class = "${classlist}" style = "height: ${height+20}px; top: ${top-10}px;"><div>${elem.name}</div></div>`);
+      }
     }
   }
 }
@@ -44,3 +42,31 @@ c2.addEventListener("change", () => hoverAction());
 
 const c3 = document.getElementById("c3");
 c3.addEventListener("change", () => hoverAction());
+
+
+
+const quizEvent = () => {
+  const $options = document.querySelectorAll(".quiz li");
+
+  for(const option of $options) {
+    option.addEventListener("click", () => {
+      const commentary = option.parentNode.nextElementSibling;
+
+      if(!commentary.classList.contains("commentary-open")) {
+        if(option.classList.contains("correct-answer")) {
+          option.classList.add("correct");
+        } else {
+          option.classList.add("incorrect");
+        }
+        commentary.classList.add("commentary-open");
+        commentary.style.height = commentary.scrollHeight + 40 + "px";
+        commentary.previousElementSibling.classList.add("open");
+        setTimeout(() => {
+          commentary.style.height = "auto";
+        }, 800);
+      }
+    });
+  }
+}
+
+quizEvent();
