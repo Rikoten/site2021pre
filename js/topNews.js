d32c710d2c20168b5dca757d87d4b395063d79fe
soliste16 <loup.solitaire1192@gmail.com>
2021-05-25 22:52:48 +0900

    各お知らせページ、メニュー実装

---
+++ js/topNews.js
@@ -0,0 +1,40 @@
+const labelName = {
+  "重要": "important",
+  "参加団体": "participants",
+  "協賛": "sponsor"
+};
+
+const latestNews = {
+  date: document.querySelector(".latest-news .date"),
+  label: document.querySelector(".latest-news .label"),
+  title: document.querySelector(".latest-news .title")
+};
+
+(async () => {
+  const json = await fetch("/data/news.json").then(res => res.json()).then(res => res.reverse());
+  const latest = json[0];
+
+  latestNews.date.textContent = latest.date;
+  latestNews.title.textContent = latest.head;
+  latestNews.label.textContent = latest.label;
+  latestNews.label.classList.add(labelName[latest.label]);
+
+  const ul = document.querySelector(".news ul");
+  const html = [];
+  let id = json.length - 1;
+  for(const data of json) {
+    html.push(`
+      <li>
+        <a href = "/news/?id=${id--}">
+          <span class = "date">${data.date}</span>
+          <div>
+            <span class = "label ${labelName[data.label]}">${data.label}</span>
+          </div>
+          <span class = "title">${data.head}</span>
+        </a>
+      </li>
+    `)
+  }
+
+  ul.innerHTML = html.join("");
+})();
